465c320fe4eb54c4bdac8812100d3362c9db61fe
eduardo-dev <eduardopichler@gmail.com>
2020-09-12 13:43:22 +0200

    Initial page.

--- src/main/kotlin/com/hedvig/controller/HtmlController.kt
+++ src/main/kotlin/com/hedvig/controller/HtmlController.kt
@@ -11,8 +11,12 @@ class HtmlController {
 
     val contractModel = ContractModel(EventsRepository().findAll());
 
-    @GetMapping("/task1")
-    fun task1(): String {
+    @GetMapping("/")
+    fun render() : String{
+        return tableTask1() + "<p/>" + tableTask2()
+    }
+
+    fun tableTask1(): String {
         val activeContracts = contractModel.getActiveContracts()
         val agwp = contractModel.getAGWP()
         val egwp = contractModel.getEGWP()
@@ -89,9 +93,81 @@ class HtmlController {
         """
     }
 
-    @GetMapping("/task2")
-    fun task2(): String {
-        return "oi"
+    fun tableTask2(): String {
+        val activeContracts = contractModel.getActiveContracts()
+        val agwpFull = contractModel.getAGWPFull()
+        val egwpFull = contractModel.getEGWPFull()
+        return """
+            <table>
+                <thead> 
+                    <tr>
+                        <th colspan="13">Task 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style="text-align: center;">
+                        <td></td>
+                        <td>January</td>
+                        <td>February</td>
+                        <td>March</td>
+                        <td>April</td>
+                        <td>May</td>
+                        <td>June</td>
+                        <td>July</td>
+                        <td>August</td>
+                        <td>September</td>
+                        <td>October</td>
+                        <td>November</td>
+                        <td>December</td>
+                    </tr>
+                    <tr style="text-align: center;">
+                        <td>Number of contracts</td>
+                        <td>${activeContracts.getValue(Month.JANUARY).size}</td>
+                        <td>${activeContracts.getValue(Month.FEBRUARY).size}</td>
+                        <td>${activeContracts.getValue(Month.MARCH).size}</td>
+                        <td>${activeContracts.getValue(Month.APRIL).size}</td>
+                        <td>${activeContracts.getValue(Month.MAY).size}</td>
+                        <td>${activeContracts.getValue(Month.JUNE).size}</td>
+                        <td>${activeContracts.getValue(Month.JULY).size}</td>
+                        <td>${activeContracts.getValue(Month.AUGUST).size}</td>
+                        <td>${activeContracts.getValue(Month.SEPTEMBER).size}</td>
+                        <td>${activeContracts.getValue(Month.OCTOBER).size}</td>
+                        <td>${activeContracts.getValue(Month.NOVEMBER).size}</td>
+                        <td>${activeContracts.getValue(Month.DECEMBER).size}</td>
+                    </tr>
+                    <tr style="text-align: center;">
+                        <td>AGWP</td>
+                        <td>${agwpFull.getValue(Month.JANUARY)}</td>
+                        <td>${agwpFull.getValue(Month.FEBRUARY)}</td>
+                        <td>${agwpFull.getValue(Month.MARCH)}</td>
+                        <td>${agwpFull.getValue(Month.APRIL)}</td>
+                        <td>${agwpFull.getValue(Month.MAY)}</td>
+                        <td>${agwpFull.getValue(Month.JUNE)}</td>
+                        <td>${agwpFull.getValue(Month.JULY)}</td>
+                        <td>${agwpFull.getValue(Month.AUGUST)}</td>
+                        <td>${agwpFull.getValue(Month.SEPTEMBER)}</td>
+                        <td>${agwpFull.getValue(Month.OCTOBER)}</td>
+                        <td>${agwpFull.getValue(Month.NOVEMBER)}</td>
+                        <td>${agwpFull.getValue(Month.DECEMBER)}</td>
+                    </tr>
+                    <tr style="text-align: center;">
+                        <td>EGWP</td>
+                        <td>${egwpFull.getValue(Month.JANUARY)}</td>
+                        <td>${egwpFull.getValue(Month.FEBRUARY)}</td>
+                        <td>${egwpFull.getValue(Month.MARCH)}</td>
+                        <td>${egwpFull.getValue(Month.APRIL)}</td>
+                        <td>${egwpFull.getValue(Month.MAY)}</td>
+                        <td>${egwpFull.getValue(Month.JUNE)}</td>
+                        <td>${egwpFull.getValue(Month.JULY)}</td>
+                        <td>${egwpFull.getValue(Month.AUGUST)}</td>
+                        <td>${egwpFull.getValue(Month.SEPTEMBER)}</td>
+                        <td>${egwpFull.getValue(Month.OCTOBER)}</td>
+                        <td>${egwpFull.getValue(Month.NOVEMBER)}</td>
+                        <td>${egwpFull.getValue(Month.DECEMBER)}</td>
+                    </tr>
+                </tbody>
+            </table>
+        """
     }
 
 }
